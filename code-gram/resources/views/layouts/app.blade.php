7c4feeb85e579c86c77974357e347caac7c56388
Sloan <Sloan.boyce@gmail.com>
2022-05-09 13:26:15 -0400

    added styles and a basic layout for the app

--- code-gram/resources/views/layouts/app.blade.php
+++ code-gram/resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    <div class="pl-4" style="border-left: 1px solid #333">codeGram</div>
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
