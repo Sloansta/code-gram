e39070fcc9f192fa66eb419a175ca165e3e584de
Sloan <Sloan.boyce@gmail.com>
2022-05-10 19:01:57 -0400

    made the profile model and added migration

--- code-gram/resources/views/home.blade.php
+++ code-gram/resources/views/home.blade.php
@@ -7,7 +7,7 @@
             <img src="https://duckduckgo.com/i/4bf02e97.png" class="rounded-circle">
         </div>
         <div class="col-9 pt-5">
-            <div><h1>codeGram</h1></div>
+            <div><h1>{{ $user->username }}</h1></div>
             <div class="d-flex">
                 <div class="pr-5"><strong>153 </strong> posts </div>
                 <div class="pr-5"><strong>23k </strong> followers </div>
