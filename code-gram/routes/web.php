1b931fdaf3b52d8ecbb412c9e78fe95dac231d37
Sloan <Sloan.boyce@gmail.com>
2022-05-14 20:05:44 -0400

    made postscontroller controller, made post model and migration, made seperate folders for post views and profile views

--- code-gram/routes/web.php
+++ code-gram/routes/web.php
@@ -20,4 +20,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
+Route::get('/p', [\App\Http\Controllers\PostsController::class, 'create']);
+
 Route::get('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
